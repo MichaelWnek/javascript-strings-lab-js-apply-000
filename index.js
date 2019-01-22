15968f41bdb792e2f4e9ccece86a925727ac4944
incendiary-agent-1085 <michaelwnek1@gmail.com>
2019-01-22 20:36:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 var greeting = "Hello, everybody!";
-
+ 
 var specialGuest = "Neil deGrasse Tyson"
-
-
-"Hello, " + specialGuest + "!";
-
+ 
 var greetSpecialGuest = "" + specialGuest + "!";
-
+ 
 var topic = "space";
-
+ 
 var conversation = `${topic}`;
-
